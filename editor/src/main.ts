699eb630a7de633413200be97678284368a76ce5
Mivort <lufterd@gmail.com>
2026-02-14 14:44:15 +0000

    feat: remove default data

--- editor/src/main.ts
+++ editor/src/main.ts
@@ -10,17 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const style = (cytoscape as any).stylesheet();
   const cy = cytoscape({
     container: document.getElementById('root'),
-    elements: [
-      { data: { id: 'n1', name: 'test' }, position: { x: 0, y: 0 } },
-      { data: { id: 'n2' }, position: { x: 100, y: 100 } },
-      { data: { id: 'n3', parent: 'n5' }, position: { x: 0, y: 100 } },
-      { data: { id: 'n4', parent: 'n5' }, position: { x: 0, y: 200 } },
-      { data: { id: 'n5' }, position: { x: 100, y: 200 } },
-
-      { group: 'edges', data: { id: 'n1n2', source: 'n1', target: 'n2' } },
-      { group: 'edges', data: { id: 'n1n5', source: 'n1', target: 'n5' } },
-      { group: 'edges', data: { id: 'n3n4', source: 'n3', target: 'n4' } },
-    ],
+    elements: [],
     layout: { name: 'preset' },
     style: style.selector('node[name]').css({ 'content': 'data(name)' }),
   });
