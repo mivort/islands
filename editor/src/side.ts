32d717505788dc6a37fd6a1137ddc226a215e88a
Mivort <lufterd@gmail.com>
2026-02-14 14:42:58 +0000

    feat: implement editing of name, ref and desc

--- editor/src/side.ts
+++ editor/src/side.ts
@@ -13,6 +13,7 @@ export class SidePanel {
     const nodes = this.cy.nodes(':selected');
     if (nodes.length === 0) {
       this.showId('');
+      this.hideNode();
     } else if (nodes.length === 1) {
       this.showId(nodes[0].id());
       this.showNode(nodes[0]);
@@ -35,12 +36,38 @@ export class SidePanel {
     if (this.desc) this.desc.value = node.data('desc') ?? '';
   }
 
+  /** Hide display of node data. */
+  hideNode() {
+    if (this.name) this.name.value = '';
+    if (this.ref) this.ref.value = '';
+    if (this.desc) this.desc.value = '';
+  }
+
   constructor(cy: cytoscape.Core) {
     this.cy = cy;
     this.id = document.getElementById('side-edit-id');
     this.name = document.getElementById('side-edit-name') as HTMLTextAreaElement;
     this.ref = document.getElementById('side-edit-ref') as HTMLInputElement;
     this.desc = document.getElementById('side-edit-desc') as HTMLTextAreaElement;
+
+    this.name?.addEventListener('change', (event) => {
+      const nodes = this.cy.nodes(':selected');
+      for (const node of nodes) {
+        node.data('name', (event.target as any).value);
+      }
+    });
+    this.ref?.addEventListener('change', (event) => {
+      const nodes = this.cy.nodes(':selected');
+      for (const node of nodes) {
+        node.data('ref', (event.target as any).value);
+      }
+    });
+    this.desc?.addEventListener('change', (event) => {
+      const nodes = this.cy.nodes(':selected');
+      for (const node of nodes) {
+        node.data('desc', (event.target as any).value);
+      }
+    });
 
     document.getElementById('side-fit-button')?.addEventListener('click', () => {
       cy.fit();
