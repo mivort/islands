07f04c6a6bb457a6ebb884ed04b3a5774cdd43f1
Mivort <lufterd@gmail.com>
2026-02-14 13:48:49 +0000

    feat: display node main data fields

--- editor/side.ts
+++ editor/side.ts
@@ -4,6 +4,9 @@ import cytoscape from 'cytoscape';
 export class SidePanel {
   cy: cytoscape.Core;
   id: HTMLElement | null;
+  name: HTMLTextAreaElement | null;
+  ref: HTMLInputElement | null;
+  desc: HTMLTextAreaElement | null;
 
   /** Update the display of selected nodes. */
   showSelected() {
@@ -12,8 +15,10 @@ export class SidePanel {
       this.showId('');
     } else if (nodes.length === 1) {
       this.showId(nodes[0].id());
+      this.showNode(nodes[0]);
     } else {
       this.showId(`<${nodes.length} selected>`);
+      this.showNode(nodes[0]);
     }
   }
 
@@ -23,9 +28,19 @@ export class SidePanel {
     this.id.innerText = id;
   }
 
+  /** Show specified node's attached data. */
+  showNode(node: cytoscape.NodeSingular) {
+    if (this.name) this.name.value = node.data('name') ?? '';
+    if (this.ref) this.ref.value = node.data('ref') ?? '';
+    if (this.desc) this.desc.value = node.data('desc') ?? '';
+  }
+
   constructor(cy: cytoscape.Core) {
     this.cy = cy;
     this.id = document.getElementById('side-edit-id');
+    this.name = document.getElementById('side-edit-name') as HTMLTextAreaElement;
+    this.ref = document.getElementById('side-edit-ref') as HTMLInputElement;
+    this.desc = document.getElementById('side-edit-desc') as HTMLTextAreaElement;
 
     document.getElementById('side-fit-button')?.addEventListener('click', () => {
       cy.fit();
