5136a3897d853b6925fc08eac5d9ca923ea8e7b5
Mivort <lufterd@gmail.com>
2026-02-16 18:54:03 +0000

    chore(sync-lsp): remove lsp-server dependency

--- sync-lsp/src/main.rs
+++ sync-lsp/src/main.rs
@@ -1,10 +1,14 @@
 use std::process::{Command, Stdio};
 
+use lsp_types::InitializeParams;
+
 fn main() -> anyhow::Result<()> {
     let mut _child = Command::new("rust-analyzer")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()?;
+
+    let _init_params = InitializeParams::default();
 
     Ok(())
 }
